d1708b86ba538c967e03402d924753b6a8d43a76
Salman Hasan [Salman Zuberi Hussain] <s.hasan@unguarded.com>
2023-11-07 14:03:35 -0500

    Add files via upload

--- UG/Screens/HomeView.swift
+++ UG/Screens/HomeView.swift
@@ -8,20 +8,42 @@
 import SwiftUI
 
 struct HomeView: View {
-   
     @AppStorage("onboarding") var isOnboardingViewActive: Bool = false
     
     var body: some View{
-    VStack(spacing:20){
-        Text("Home")
-            .font(.largeTitle)
-        
-        Button(action:{
-           isOnboardingViewActive = true
-        }){
-            Text("Restart")
-        }
+      VStack(spacing:20){
+          
+        Spacer()
+          
+        Image("character-2")
+              .resizable()
+              .scaledToFit()
+              .padding()
+          
+        Text("The time that leads to mastery is dependent on the intensiy of our focus.")
+              .font(.title3)
+              .fontWeight(.light)
+              .foregroundColor(.secondary)
+              .multilineTextAlignment(.center)
+              .padding()
+          
+          
+        Spacer()
+          
+    Button(action:{
+        isOnboardingViewActive = true
+    }) {
+    Image(systemName: "arrow.triangle.2.circlepath.circle.fill")
+        .imageScale(.large)
+            
+    Text("Restart")
+        .font(.system(.title3, design:.rounded))
+        .fontWeight(.bold)
     }
+       .buttonStyle(.borderedProminent)
+       .buttonBorderShape(.capsule)
+       .controlSize(.large)
+     }
   }
 }
 
--- UG/Screens/OnboardingView.swift
+++ UG/Screens/OnboardingView.swift
@@ -36,18 +36,13 @@ struct OnboardingView: View {
                     .padding(.horizontal,10)
                 }
         ZStack {
-            ZStack{
-                Circle()
-                    .stroke(.white.opacity(0.2), lineWidth:40)
-                    .frame(width:260, height:260, alignment:.center)
-                Circle()
-                    .stroke(.white.opacity(0.2), lineWidth: 80)
-                    .frame(width:260, height:260, alignment:.center)
-                }
+            CircleGroupView(ShapeColor: .white, ShapeOpacity: 0.2)
+           
             Image("character-1")
                 .resizable()
                 .scaledToFit()
                 }
+            
             Spacer()
             
             ZStack{
