083eb52bae079ab5bc573a797f2f350bc29d1f90
Salman Hasan [Salman Zuberi Hussain] <s.hasan@unguarded.com>
2023-11-07 10:38:43 -0500

    Add files via upload

--- UG/Screens/OnboardingView.swift
+++ UG/Screens/OnboardingView.swift
@@ -7,21 +7,102 @@
 
 import SwiftUI
 
-struct OnboardingView: View{
-    @AppStorage("onboarding") var isOnboardingViewActive: Bool = true
+struct OnboardingView: View {
+    // MARK: - PROPERTY
     
-    //MARK: -BODY
+    @AppStorage("onboarding") var isOnboardingViewActive: Bool = true
+   
+    // MARK: - BODY
     
     var body: some View {
-        VStack(spacing:20) {
-            Text("Onboarding")
-                .font(.largeTitle)
+        ZStack {
+          Color("ColorBlack")
+             .ignoresSafeArea(.all, edges: .all)
             
-            Button(action:{
-                isOnboardingViewActive = false
-            }){
-                Text("Start")
+        VStack(spacing: 20) {
+                
+            Spacer()
+                
+            VStack(spacing:0) {
+                Text("UNGUARDED.")
+                    .font(.system(size: 40))
+                    .fontWeight(.heavy)
+                    .foregroundColor(.white)
+                Text("It's not how much we give but how much love we put into giving.")
+                    .font(.title3)
+                    .fontWeight(.light)
+                    .foregroundColor(.white)
+                    .multilineTextAlignment(.center)
+                    .padding(.horizontal,10)
+                }
+        ZStack {
+            ZStack{
+                Circle()
+                    .stroke(.white.opacity(0.2), lineWidth:40)
+                    .frame(width:260, height:260, alignment:.center)
+                Circle()
+                    .stroke(.white.opacity(0.2), lineWidth: 80)
+                    .frame(width:260, height:260, alignment:.center)
+                }
+            Image("character-1")
+                .resizable()
+                .scaledToFit()
+                }
+            Spacer()
+            
+            ZStack{
+                Capsule()
+                    .fill(Color.white.opacity(0.2))
+                
+                Capsule()
+                    .fill(Color.white.opacity(0.2))
+                    .padding(8)
+                
+                
+                Text("Get Started")
+                    .font(.system(.title3, design: .rounded))
+                    .fontWeight(.bold)
+                    .foregroundColor(.white)
+                    .offset(x:20)
+                
+                
+                HStack{
+                    Capsule()
+                        .fill(Color("ColorOrange"))
+                        .frame(width:80)
+                    
+                    Spacer()
+                    
+                }
+                HStack {
+                    ZStack{
+                        Circle()
+                            .fill(Color("ColorOrange"))
+                        Circle()
+                            .fill(.black.opacity(0.15))
+                            .padding(8)
+                        Image(systemName:"chevron.right.2")
+                            .font(.system(size:24, weight:.bold))
+                    }
+                .foregroundColor(.white)
+                .frame(width: 80, height: 80, alignment: .center)
+                .onTapGesture{
+                    isOnboardingViewActive = false
+                }
+                    Spacer()
+                } //: HSTACK
             }
-        }
+            .frame(height:80, alignment: .center)
+            .padding()
+          }
+       } //: HEADER
     }
 }
+        
+struct OnboardingView_Previews: PreviewProvider {
+  static var previews: some View {
+    OnboardingView()
+  }
+}
+
+
