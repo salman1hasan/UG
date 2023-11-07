02e90b8b74914aeef048f905d14e43fde6b3c580
Salman Hasan [Salman Zuberi Hussain] <s.hasan@unguarded.com>
2023-11-07 15:30:09 -0500

    Add files via upload

--- UG/Screens/OnboardingView.swift
+++ UG/Screens/OnboardingView.swift
@@ -11,6 +11,9 @@ struct OnboardingView: View {
     // MARK: - PROPERTY
     
     @AppStorage("onboarding") var isOnboardingViewActive: Bool = true
+    
+    @State private var buttonWidth: Double = UIScreen.main.bounds.width - 80
+    @State private var buttonOffset: CGFloat = 0
    
     // MARK: - BODY
     
@@ -54,40 +57,53 @@ struct OnboardingView: View {
                     .padding(8)
                 
                 
-                Text("Get Started")
+        Text("Get Started")
                     .font(.system(.title3, design: .rounded))
                     .fontWeight(.bold)
                     .foregroundColor(.white)
                     .offset(x:20)
                 
                 
-                HStack{
-                    Capsule()
-                        .fill(Color("ColorOrange"))
-                        .frame(width:80)
+        HStack{
+            Capsule()
+                .fill(Color("ColorOrange"))
+                .frame(width: buttonOffset + 80)
                     
-                    Spacer()
-                    
-                }
-                HStack {
-                    ZStack{
-                        Circle()
-                            .fill(Color("ColorOrange"))
-                        Circle()
-                            .fill(.black.opacity(0.15))
-                            .padding(8)
-                        Image(systemName:"chevron.right.2")
-                            .font(.system(size:24, weight:.bold))
+            Spacer()
+    }
+    HStack {
+        ZStack{
+            Circle()
+             .fill(Color("ColorOrange"))
+            Circle()
+            .fill(.black.opacity(0.15))
+            .padding(8)
+        Image(systemName:"chevron.right.2")
+            .font(.system(size:24, weight:.bold))
+        }
+    .foregroundColor(.white)
+    .frame(width: 80, height: 80, alignment: .center)
+        .offset(x: buttonOffset)
+        .gesture(
+         DragGesture()
+          .onChanged { gesture in
+          if gesture.translation.width > 0 && buttonOffset <= buttonWidth - 80 {
+        buttonOffset = gesture.translation.width
+                        }
                     }
-                .foregroundColor(.white)
-                .frame(width: 80, height: 80, alignment: .center)
-                .onTapGesture{
+            .onEnded{ _ in
+                if buttonOffset > buttonWidth / 2 {
+                    buttonOffset = buttonWidth - 80
                     isOnboardingViewActive = false
+                } else {
+                    buttonOffset = 0
                 }
-                    Spacer()
+            }
+        )
+                Spacer()
                 } //: HSTACK
             }
-            .frame(height:80, alignment: .center)
+            .frame(width: buttonWidth, height:80, alignment: .center)
             .padding()
           }
        } //: HEADER
