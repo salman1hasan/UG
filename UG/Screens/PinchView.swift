00e94edce33e3165accc4f878dc2b06a13bade65
Salman Hasan [Salman Zuberi Hussain] <s.hasan@unguarded.com>
2023-11-11 12:49:11 -0500

    Add files via upload

---
+++ UG/Screens/PinchView.swift
@@ -0,0 +1,83 @@
+//
+//  ContentView.swift
+//  Pinch
+//
+//  Created by Salman Hasan on 11/10/23.
+//
+
+import SwiftUI
+
+struct PinchView: View {
+    
+    @State private var isAnimating: Bool = false
+    @State private var imageScale: CGFloat = 1
+    @State private var imageOffset: CGSize = .zero
+    
+    
+    func resetImageState(){
+        return withAnimation(.spring()){
+            imageScale = 1
+            imageOffset = .zero
+        }
+    }
+    
+    var body: some View {
+        NavigationView{
+        ZStack{
+            Color.clear
+              
+            Image("UNGUARDED")
+                .resizable()
+                .aspectRatio(contentMode:.fit)
+                .cornerRadius(10)
+                .padding()
+                .shadow(color:.black.opacity(0.2), radius:12, x:2, y:2)
+                .opacity(isAnimating ? 1:0)
+                .offset(x: imageOffset.width, y: imageOffset.height)
+                .scaleEffect(imageScale)
+                .onTapGesture(count:2, perform: {
+                    if imageScale == 1 {
+                        withAnimation(.spring()){
+                            imageScale = 5
+                        }
+                    }else{
+                        withAnimation(.spring()){
+                           resetImageState()
+                        }
+                    }
+                })
+        .gesture(
+            DragGesture()
+            .onChanged { value in
+            withAnimation(.linear(duration: 1)){
+            imageOffset = value.translation
+                }
+              }
+            .onEnded { _ in
+                if imageScale <= 1 {
+                    resetImageState()
+                    }
+                }
+           )
+        }
+            .navigationTitle("Pinch & Zoom")
+            .navigationBarTitleDisplayMode(.inline)
+            .onAppear(perform: {
+                withAnimation(.linear(duration:1)){
+                     isAnimating = true
+                }
+            })
+            .overlay(
+        InfoPanelView(scale: imageScale, offset: imageOffset)
+            .padding(.horizontal)
+            .padding(.top,30)
+        , alignment: .top
+            )
+        }
+        .navigationViewStyle(.stack)
+        }
+}
+
+#Preview {
+    PinchView()
+}
