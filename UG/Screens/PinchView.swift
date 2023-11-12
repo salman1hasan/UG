38ad7275c158238fd5d2d2559589fd946b3390fb
Salman Hasan [Salman Zuberi Hussain] <s.hasan@unguarded.com>
2023-11-12 09:26:09 -0500

    Add files via upload

--- UG/Screens/PinchView.swift
+++ UG/Screens/PinchView.swift
@@ -14,8 +14,7 @@ struct PinchView: View {
     @State private var imageScale: CGFloat = 1
     @State private var imageOffset: CGSize = .zero
     @State private var isDrawerOpen: Bool = false
-    
-    
+
     func resetImageState(){
         return withAnimation(.spring()){
             imageScale = 1
@@ -25,144 +24,122 @@ struct PinchView: View {
     
     var body: some View {
         NavigationView{
-            ZStack{
-                Color.clear
-                
-                Image("UNGUARDED1")
-                    .resizable()
-                    .aspectRatio(contentMode:.fit)
-                    .cornerRadius(10)
-                    .padding()
-                   
-                    .opacity(isAnimating ? 1:0)
-                    .offset(x: imageOffset.width, y: imageOffset.height)
-                    .scaleEffect(imageScale)
-                    .onTapGesture(count:2, perform: {
-                        if imageScale == 1 {
-                            withAnimation(.spring()){
-                                imageScale = 5
-                            }
-                        }else{
-                            withAnimation(.spring()){
-                                resetImageState()
-                            }
-                        }
-                    })
-                    .gesture(
-                        DragGesture()
-                            .onChanged { value in
-                                withAnimation(.linear(duration: 1)){
-                                    imageOffset = value.translation
-                                }
-                            }
-                            .onEnded { _ in
-                                if imageScale <= 1 {
-                                    resetImageState()
-                                }
-                            }
-                    )
+            
+          ZStack{
+
+            Color.clear
+              Color("ColorOrange")
+                  .ignoresSafeArea(.all, edges: .all)
+              
+            Image("thumb-magazine-front-cover")
+                .resizable()
+                .aspectRatio(contentMode:.fit)
+                .cornerRadius(8)
+                .padding()
+                .opacity(isAnimating ? 1:0)
+                .offset(x: imageOffset.width, y:
+                imageOffset.height)
+                .scaleEffect(imageScale)
+                .onTapGesture(count:2, perform: {
+                if imageScale == 1 {
+                withAnimation(.spring()){
+                imageScale = 5
+                }
+                }else{
+                withAnimation(.spring()){
+                resetImageState()
+                }
+                }
+                })
+                .gesture(
+                DragGesture()
+                .onChanged { value in
+                withAnimation(.linear(duration: 1)){
+                imageOffset = value.translation
+                }
+                }
+                .onEnded { _ in
+                if imageScale <= 1 {
+                resetImageState()
+                }
+            }
+        )
     .gesture(
         MagnificationGesture()
             .onChanged { value in
         withAnimation(.linear(duration: 1)){
         if imageScale >= 1 && imageScale <= 5 {
         imageScale = value
-                                    } else if imageScale > 5 {
-                                        imageScale = 5
-                                    }
-                                }
-                            }
-                            .onEnded { _ in
-                                if imageScale > 5 {
-                                    imageScale = 5
-                                } else if imageScale <= 1 {
-                                    resetImageState()
-                                }
-                            }
-                    )
-            }
-            .navigationTitle("Pinch & Zoom")
-            .navigationBarTitleDisplayMode(.inline)
-            .onAppear(perform: {
-                withAnimation(.linear(duration:1)){
-                    isAnimating = true
-                }
-            })
-            .overlay(
-                InfoPanelView(scale: imageScale, offset: imageOffset)
-                    .padding(.horizontal)
-                    .padding(.top,30)
-                , alignment: .top
-            )
-            .overlay(
-                Group{
-                    HStack{
-                        Button{
-                            withAnimation(.spring()){
-                                if imageScale > 1 {
-                                    imageScale -= 1
-                                    if imageScale <= 1 {
-                                        resetImageState()
-                                    }
-                                }
-                            }
-                        }label:{
-                            ControlImageView(icon: "minus.magnifyingglass")
-                        }
+        } else if imageScale > 5 {
+        imageScale = 5
+        }
+        }
+        }
+       .onEnded { _ in
+        if imageScale > 5 {
+        imageScale = 5
+        } else if imageScale <= 1 {
+        resetImageState()
+        }
+        }
+        )
+        }
+        .navigationBarTitleDisplayMode(.inline)
+        .onAppear(perform: {
+        withAnimation(.linear(duration:1)){
+        isAnimating = true
+        }
+})
+        .overlay(
+        InfoPanelView(scale: imageScale, offset: imageOffset)
+        .padding(.horizontal)
+        .padding(.top,30)
+        , alignment: .top
+        )
+        .overlay(
+        Group{
+        HStack{
+        Button{
+        withAnimation(.spring()){
+        if imageScale > 1 {
+        imageScale -= 1
+        if imageScale <= 1 {
+        resetImageState()
+        }
+    }
+}
+        }label:{
+        ControlImageView(icon: "minus.magnifyingglass")
+}
                         
-                        Button{
-                            resetImageState()
-                        }label:{
-                            ControlImageView(icon: "arrow.up.left.and.down.right.magnifyingglass")
-                        }
+        Button{
+        resetImageState()
+        }label:{
+        ControlImageView(icon: "arrow.up.left.and.down.right.magnifyingglass")
+        }
                         
-                        Button{
-                            withAnimation(.spring()){
-                                if imageScale < 5 {
-                                    imageScale += 1
-                                    if imageScale > 5 {
-                                        imageScale = 5
-                                    }
-                                }
-                            }
-                        }label:{
-                            ControlImageView(icon: "plus.magnifyingglass")
-                                .font(.system(size:36))
-                        }
-                    }
-                    .padding(EdgeInsets(top:12, leading:20, bottom: 12, trailing: 20))
-                    .background(.ultraThinMaterial)
-                    .cornerRadius(12)
-                    .opacity(isAnimating ? 1 : 0)
-                }
-                    .padding(.bottom, 30)
-                , alignment: .bottom
-            )
-            .overlay(
-                HStack(spacing: 12){
-                    Image(systemName: isDrawerOpen ? "chevron.compact.right" : "chevron.compact.left")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(height: 40)
-                        .padding(8)
-                        .foregroundStyle(.secondary)
-                        .onTapGesture(perform: {
-                            withAnimation(.easeOut){
-                             isDrawerOpen.toggle()
-                            }
-                        })
-                        
-                    Spacer()
-                }
-                    .padding(EdgeInsets(top: 16, leading: 8, bottom: 16, trailing: 8))
-                    .background(.ultraThinMaterial)
-                    .cornerRadius(12)
-                    .opacity(isAnimating ? 1 : 0)
-                    .frame(width: 260)
-                    .padding(.top,UIScreen.main.bounds.height / 12)
-                    .offset(x: isDrawerOpen ? 20 : 215)
-                ,   alignment: .topTrailing
-            )
+        Button{
+        withAnimation(.spring()){
+        if imageScale < 5 {
+        imageScale += 1
+        if imageScale > 5 {
+        imageScale = 5
+        }
+        }
+        }
+        }label:{
+        ControlImageView(icon: "plus.magnifyingglass")
+        .font(.system(size:36))
+        }
+}
+        .padding(EdgeInsets(top:12, leading:20, bottom: 12, trailing: 20))
+        .background(.ultraThinMaterial)
+        .cornerRadius(12)
+            .opacity(isAnimating ? 1 : 0)
+        }
+            .padding(.bottom, 30)
+            , alignment: .bottom
+        )
         }
         .navigationViewStyle(.stack)
         }
@@ -171,7 +148,7 @@ struct PinchView: View {
 struct PinchView_Previews: PreviewProvider{
     static var previews: some View{
         PinchView()
-        
+
     }
 }
 
